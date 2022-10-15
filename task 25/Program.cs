ac193558a13cbfc53d3991c770c109c16a56a0ea
Natalia P <nspant@yandex.ru>
2022-10-15 13:54:56 +0300

    End Task 25

--- task 25/Program.cs
+++ task 25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int numa, numb, rst;
+
+Console.Write("Введите число A: ");
+numa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+numb = Convert.ToInt32(Console.ReadLine());
+    rst = numa;
+        for (int i = 2; i <= numb; i++)
+            {
+                rst *= numa;
+            }
+Console.WriteLine($"{numa}^{numb} = {rst}");
+
