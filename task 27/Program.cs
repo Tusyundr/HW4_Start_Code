2b7226eac5b890bd83516273423cbd0bfb409910
Natalia P <nspant@yandex.ru>
2022-10-15 14:05:00 +0300

    End Task 27

--- task 27/Program.cs
+++ task 27/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num, rst;
+
+Console.Write("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+    rst = 0;
+    while (num != 0)
+    {
+        rst += num % 10;
+        num /= 10;
+    }
+
+Console.WriteLine($"Сумма цифр в числе {num} = {rst}");
